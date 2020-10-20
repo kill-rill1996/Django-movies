c015c99c0a8edae1f5a792c5da5455f814bc91bc
Kirill Andreev <Hizenberg228@,ail.ru>
2020-10-20 22:02:55 +0300

    Add template tags

--- movies/core/urls.py
+++ movies/core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MoviesView.as_view()),
+    path('', views.MoviesView.as_view(), name='home-page'),
     path('<slug:slug>/', views.MoviewDetailView.as_view(), name='movie-detail'),
     path('review/<int:pk>/', views.ReviewView.as_view(), name='add-review'),
 
--- movies/core/views.py
+++ movies/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
-from .models import Movie
+from .models import Movie, Category
 from .forms import ReviewForm
 
 
@@ -11,12 +11,22 @@ class MoviesView(ListView):
     queryset = Movie.objects.filter(draft=False)
     template_name = "movies/movies.html"
 
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     context['categories'] = Category.objects.all()
+    #     return context
+
 
 class MoviewDetailView(DetailView):
     """Film detail, full description"""
     model = Movie
     slug_field = "url"
     template_name = "movies/movie_detail.html"
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super().get_context_data(*args, **kwargs)
+    #     context['categories'] = Category.objects.all()
+    #     return context
 
 
 class ReviewView(View):
